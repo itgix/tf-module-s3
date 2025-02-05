1ae02f21030b29d062b912313247653061bd882f
Tsvetana Kanzova <tsvetana.kanzova@itgix.com>
2025-02-05 17:35:23 +0200

    Modified locals.tf file

--- locals.tf
+++ locals.tf
@@ -24,38 +24,6 @@ locals {
     "us-west-2"      = "uw2"
   }
 
-  bucket_aws_regions_short = {
-    "ap-east-1"      = "ae1"
-    "ap-northeast-1" = "ane1"
-    "ap-northeast-2" = "ane2"
-    "ap-northeast-3" = "ane3"
-    "ap-south-1"     = "as1 "
-    "ap-southeast-1" = "ase1"
-    "ap-southeast-2" = "ase2"
-    "ca-central-1"   = "cc1"
-    "eu-central-1"   = "ec1"
-    "eu-north-1"     = "en1"
-    "eu-south-1"     = "es1"
-    "eu-west-1"      = "ew1"
-    "eu-west-2"      = "ew2"
-    "eu-west-3"      = "ew3"
-    "af-south-1"     = "fs1"
-    "me-south-1"     = "ms1"
-    "sa-east-1"      = "se1"
-    "us-east-1"      = "ue1"
-    "us-east-2"      = "ue2"
-    "us-west-1"      = "uw1"
-    "us-west-2"      = "uw2"
-  }
-
-  environment_short = {
-    "dev"       = "d"
-    "prod"      = "p"
-    "test"      = "t"
-    "sandbox"   = "s"
-  }
-
-
   bucket_name_prefix  = "{var.app}-s3-${local.aws_regions_short[var.aws_region]}-${var.environment}"
 
 }
